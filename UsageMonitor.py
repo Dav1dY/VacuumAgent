aade8fcff6ec5a4e873f155dddf1df18616920a8
Dav1dY <yuanye_avalon@hotmail.com>
2023-10-26 10:22:19 +0800

    10.26.1
    change search station name to search local ip

--- UsageMonitor.py
+++ UsageMonitor.py
@@ -10,24 +10,24 @@ import psutil
 import os
 
 
-class SystemInfo:
+class UsageMonitor:
     def __init__(self):
         self.init_success = False
 
-        self.logger = logging.getLogger('SystemInfoLogger')
+        self.logger = logging.getLogger('UsageMonitorLogger')
         self.logger.setLevel(logging.INFO)
 
-        dir_name = '/vault/SystemInfo/log'
+        dir_name = '/vault/UsageMonitor/log'
         if not os.path.exists(dir_name):
             # noinspection PyBroadException
             try:
                 os.makedirs(dir_name)
-            except Exception as e:
+            except Exception:
                 print("Can not create log file, exit.")
                 return
         # noinspection PyBroadException
         try:
-            handler = TimedRotatingFileHandler('/vault/SystemInfo/log/SystemInfo.log', when='midnight', backupCount=30)
+            handler = TimedRotatingFileHandler('/vault/UsageMonitor/log/UsageMonitor.log', when='midnight', backupCount=30)
         except Exception:
             print("Logger error, exit.")
             return
@@ -42,11 +42,11 @@ class SystemInfo:
         self.loaded_data = None
         # noinspection PyBroadException
         try:
-            with open('/vault/SystemInfo/config/SystemInfo_config.json', 'r') as f:
+            with open('/vault/UsageMonitor/config/UsageMonitor_config.json', 'r') as f:
                 self.loaded_data = json.load(f)
-            self.logger.info("SystemInfo_config load success.")
+            self.logger.info("UsageMonitor_config load success.")
         except FileNotFoundError:
-            self.logger.error("SystemInfo_config was not found.")
+            self.logger.error("UsageMonitor_config was not found.")
             return
         except json.JSONDecodeError:
             self.logger.error("An error occurred while decoding the JSON.")
@@ -74,41 +74,64 @@ class SystemInfo:
             self.cpu_message = None
             self.ram_message = None
             self.disk_usage = None
-            try:
-                with open('/vault/data_collection/test_station_config/gh_station_info.json', 'r') as f:
-                    data = json.load(f)
-                    if 'ghinfo' in data and 'STATION_NUMBER' in data['ghinfo']:
-                        self.station_number = data['ghinfo']['STATION_NUMBER']
-                    else:
-                        self.logger.error('Cannot find STATION_NUMBER.')
-                        return
-                    if 'ghinfo' in data and 'STATION_TYPE' in data['ghinfo']:
-                        self.station_type = data['ghinfo']['STATION_TYPE']
-                    else:
-                        self.logger.error('Cannot find STATION_TYPE.')
-                        return
-            except FileNotFoundError:
-                self.logger.error('File gh_station_info.json not found.')
+            self.maincomponent_id = None
+
+            for interface, addrs in psutil.net_if_addrs().items():
+                for addr in addrs:
+                    if addr.address == '10.0.1.200':
+                        try:
+                            with open('/vault/ADCAgent/dst/setting/adc_agent/register.json', 'r') as f:
+                                data = json.load(f)
+                                if 'cell_type' in data:
+                                    self.maincomponent_id = "work_station_" + data['cell_type']
+                                    break
+                                else:
+                                    self.logger.error("Can not find cell type in register.json.")
+                                    return
+                        except Exception as e:
+                            self.logger.error(f"Failed to load adc_agent/register.json : {e}.")
+                            return
+                    elif addr.address.startswith('10.0.1.'):
+                        try:
+                            with open('/vault/data_collection/test_station_config/gh_station_info.json', 'r') as f:
+                                data = json.load(f)
+                                if 'ghinfo' in data and 'STATION_NUMBER' in data['ghinfo']:
+                                    self.station_number = data['ghinfo']['STATION_NUMBER']
+                                else:
+                                    self.logger.error('Cannot find STATION_NUMBER.')
+                                    return
+                                if 'ghinfo' in data and 'STATION_TYPE' in data['ghinfo']:
+                                    self.station_type = data['ghinfo']['STATION_TYPE']
+                                else:
+                                    self.logger.error('Cannot find STATION_TYPE.')
+                                    return
+                        except FileNotFoundError:
+                            self.logger.error('File gh_station_info.json not found.')
+                            return
+                        except json.JSONDecodeError:
+                            self.logger.error('An error occurred while decoding the JSON file.')
+                            return
+                        except Exception as e:
+                            self.logger.error(f"An error occurred : {e}.")
+                            return
+                        self.maincomponent_id = "work_station_" + self.station_type + "_" + self.station_number
+                        break
+            if self.maincomponent_id is None:
+                self.logger.error("Can not find local address.")
                 return
-            except json.JSONDecodeError:
-                self.logger.error('An error occurred while decoding the JSON file.')
-                return
-            except Exception as e:
-                self.logger.error(f"An error occurred : {e}.")
-                return
-            self.logger.info("Load station info success.")
-            if self.station_type == "QT-BCM2" or self.station_type == "BOOT-ARGS":
-                maincomponent_id = "work_station_" + self.station_type
             else:
-                maincomponent_id = "work_station_" + self.station_type + "_" + self.station_number
-            subcomponent = "SystemMonitor"
-            self.config_topic = "/Devices/" + maincomponent_id + "/" + subcomponent + "/" + "Config"
-            self.analog_topic = "/Devices/" + maincomponent_id + "/" + subcomponent + "/" + "Analog"
+                self.logger.info("Load station info success.")
+            subcomponent = "UsageMonitor"
+            self.config_topic = "/Devices/" + self.maincomponent_id + "/" + subcomponent + "/" + "Config"
+            self.analog_topic = "/Devices/" + self.maincomponent_id + "/" + subcomponent + "/" + "Analog"
+            self.logger.info(f"config_topic: {self.config_topic}")
+            self.logger.info(f"analog_topic: {self.analog_topic}")
+
             if self.loaded_data is None:
                 self.query_config_topic = "/Devices/adc_agent/QueryConfig"
                 self.broker = "10.0.1.200"
                 self.port = 1883
-                self.config_path = "Config2Send_Vacuum.json"
+                self.config_path = "Config2Send_UsageMonitor.json"
                 self.report_interval = 5
                 self.connect_retry_times = 3
                 self.publish_fail_tolerance = 5
@@ -143,9 +166,9 @@ class SystemInfo:
         try:
             with open(self.config_path, 'r') as f:
                 self.config_data = json.load(f)
-            self.logger.info("Config2Send_SystemInfo.json load success.")
+            self.logger.info("Config2Send_UsageMonitor.json load success.")
         except FileNotFoundError:
-            self.logger.error("Config2Send_SystemInfo.json was not found.")
+            self.logger.error("Config2Send_UsageMonitor.json was not found.")
             return
         except json.JSONDecodeError:
             self.logger.error("An error occurred while decoding the JSON.")
@@ -395,6 +418,6 @@ class SystemInfo:
 
 
 if __name__ == '__main__':
-    new = SystemInfo()
+    new = UsageMonitor()
     if new.init_success:
         new.start()
